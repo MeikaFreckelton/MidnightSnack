5376f90d5ed71f58537dd8c8aebd9bf0607c59f1
MeikaFreckelton <you@example.com>
2020-04-26 17:04:05 +1000

    finished tests, no problems

--- src/tests.rb
+++ src/tests.rb
@@ -2,14 +2,19 @@ require 'test/unit'
 require_relative './app.rb'
 
 class GameTest < Test::Unit::TestCase
-    def test_name #test if name given is a string
-        name = "meika"
-        assert_equal(name, String)
+    def test_left #testing the movement of keys
+        game = CatGame.new(board)
+        assert_equal("left", @@x-=1)
     end
 
-    def test_board #tests if the board given is in an array form
+    def test_up #tests if right will move right
         game = Catgame.new(board)
-        assert_equal(board, Array)
+        assert_equal("up", @@y-=1)
+    end
+
+    def test_keys # tests if arrow key input will move user
+        game = CatGame.new(board)
+        assert_equal("\e[A", @@y-=1)
     end
 
     def test_fail 
